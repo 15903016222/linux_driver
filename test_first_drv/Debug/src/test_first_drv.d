7b85b9b5895b7e4a080a1c3f6b6003fda7ab6cad
daysmark <912293097@qq.com>
2014-12-19 17:09:41 +0800

    LED驱动的测试程序

---
+++ test_first_drv/Debug/src/test_first_drv.d
@@ -0,0 +1,1 @@
+src/test_first_drv.d src/test_first_drv.o: ../src/test_first_drv.c
---
+++ test_first_drv/src/test_first_drv.c
@@ -0,0 +1,33 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+/*wq_device on  ´ò¿ª
+ *wq_device off ¹Ø±Õ
+ */
+int main(int argc, char **argv)
+{
+	int fd;
+	int val = 1;
+	fd = open("/dev/wq_device",	O_RDWR);
+	if(fd < 0)
+	{
+		printf("can't open \n");
+	}
+	if(argc != 2)
+	{
+		printf("Usage :\n");
+		printf("%s <on|off>\n",argv[0]);
+		return 0;
+	}
+	if(strcmp(argv[1],"on") == 0)
+	{
+		val =1;
+	}
+	else
+	{
+		val =0;
+	}
+	write(fd,&val,4);
+	return 0;
+}
